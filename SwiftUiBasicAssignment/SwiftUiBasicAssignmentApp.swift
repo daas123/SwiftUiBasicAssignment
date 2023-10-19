a11325756ac6eaaf076dc608eaf5f797c60ad8db
saad <saadneosoft@gmail.com>
2023-10-19 17:08:12 +0530

    Initial Commit

---
+++ SwiftUiBasicAssignment/SwiftUiBasicAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBasicAssignmentApp.swift
+//  SwiftUiBasicAssignment
+//
+//  Created by Neosoft on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBasicAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
